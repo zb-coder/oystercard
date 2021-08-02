4ec652eb07e641c5f243a4572588f2231fd63ed0
zainab baruud <zainabbaruud@ma373.home>
2021-08-02 18:17:05 +0100

    challenge 5: incomplete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,17 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  card = Oystercard.new
+  it 'should have a new balance' do
+  expect(card.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+    #expect(card.top_up) to increase()
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'is expected to increase balance' do
+      expect(card.balance).to increase(:top_up)
+    end
+  end
 end
