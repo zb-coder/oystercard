43ace5ae0997a9904bddf6cd08c56060f75d4af0
zainab baruud <zainabbaruud@ma373.home>
2021-08-04 12:27:25 +0100

    Challenge 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 DEFAULT_BALANCE = 0
 MAXIMUM_BALANCE = 90
+MINIMUM_BALANCE = 1
 
 class Oystercard 
-  attr_reader :balance
+  attr_reader :balance, :status
 
-  def initialize(balance = DEFAULT_BALANCE)
+  def initialize(balance = DEFAULT_BALANCE, status = false)
     @balance = balance
+    @status = status
   end
 
   def top_up(amount)
@@ -16,11 +18,35 @@ class Oystercard
   def deduct(deduction)
     @balance = @balance - deduction
   end
-    
+
+  def in_journey?
+    @status
+  end
+
+  def touch_in
+    message = "You do not have enough funds, you need atleast #{MINIMUM_BALANCE}"
+    (minimum_balance) ? (@status = true) : (raise message)
+  end
+  
+  def touch_out
+    @status = false
+  end
   private
 
   def maximum_balance(amount)
     @balance + amount <= MAXIMUM_BALANCE
   end
 
+  def minimum_balance
+    @balance >= MINIMUM_BALANCE
+  end
+
 end
+
+# card = Oystercard.new
+# puts card.status
+# puts card.touch_in
+# puts card.status
+# puts card.touch_out
+# puts card.status
+# puts card.in_journey?
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,7 @@ describe Oystercard do
   describe '#top_up' do
     #expect(card.top_up) to increase()
     it { is_expected.to respond_to(:top_up).with(1).argument }
-
+  
     it 'is expected to increase balance' do
       opening_balance = subject.balance
       subject.top_up(20)
@@ -28,4 +28,66 @@ describe Oystercard do
     #if deduct(amount) from card then balance reduced by that amount
     end
   end
+    
+  describe '#touch_in' do
+    
+    #touch_in
+    # it should return
+    # return error message when false
+    it 'is expected to have a minimum balance of £1' do
+      message = "You do not have enough funds, you need atleast #{MINIMUM_BALANCE}"
+      expect { subject.touch_in }.to raise_error message
+    end
+
+    #touch_in
+    # return true when scan card
+    #return true
+    it 'is expected to return true when touched_in' do
+      subject.top_up(10)
+      expect(subject.touch_in).to eq(true)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'is expected to return false when you touch out' do
+      expect(subject.touch_out).to eq(false)
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'is expected to return false by default' do
+      expect(subject.in_journey?).to eq(false)
+    end
+
+    it 'is expected to return true when status is true' do
+      subject.top_up(10)
+      subject.touch_in
+      expect(subject.in_journey?).to eq(true)
+    end
+
+  end
+
+    #Method
+    #What it should do?
+    #What is should return?
+
+    #in_journey?
+    # returns true or false
+    #return true 
+
+    #touch_out
+    # it changes value to false
+    # return false
+
+    #touch_in
+    # it should return
+    # return error message when false
+
+    #touch_in
+    # don't raise an error
+    # set the status to true
+
+    #Minimum_amount
+    # it should return
+    # balance >= 1
 end
